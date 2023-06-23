35180cec69723aa44662ca147530af77fb3e4cfa
sachinsoni7827 <137540808+sachinsoni7827@users.noreply.github.com>
2023-06-24 01:19:15 +0530

    Add files via upload

---
+++ 15 Company.sql
@@ -0,0 +1,21 @@
+/*
+-- Query: select * from world
+LIMIT 0, 1000
+
+-- Date: 2023-05-31 18:45
+*/
+INSERT INTO `` (`company_name`,`website`,`first_name`,`last_name`,`email`) VALUES ('Company A','https://www.companya.com','John','Doe','john.doe@example.com');
+INSERT INTO `` (`company_name`,`website`,`first_name`,`last_name`,`email`) VALUES ('Infosys A','https://www.infosys.com','Mahesh','Kumar','mahes@infosys.com');
+INSERT INTO `` (`company_name`,`website`,`first_name`,`last_name`,`email`) VALUES ('Infosys A','https://www.infosys.com','Mahesh','Kumar','mahes@infosys.com');
+INSERT INTO `` (`company_name`,`website`,`first_name`,`last_name`,`email`) VALUES ('HP','https://www.hpinc.com','Akash','Kumar','mahesh@hpinc.com');
+INSERT INTO `` (`company_name`,`website`,`first_name`,`last_name`,`email`) VALUES ('Adani','https://www.adani.com','Ashish','Soni','ashish.soni@adani.com');
+INSERT INTO `` (`company_name`,`website`,`first_name`,`last_name`,`email`) VALUES ('HCL','https://www.hcl.com','Priyanka','Tomar','priyanka@hcl.com');
+INSERT INTO `` (`company_name`,`website`,`first_name`,`last_name`,`email`) VALUES ('PWC','https://www.pwc.co.in','Saurabh','S','saurabhs@pwc.co.in');
+INSERT INTO `` (`company_name`,`website`,`first_name`,`last_name`,`email`) VALUES ('American Express','https://www.americanexpress.com','Vikrant','Gupta','vikrantgupta@americanexpress.com');
+INSERT INTO `` (`company_name`,`website`,`first_name`,`last_name`,`email`) VALUES ('Capegemini','https://www.capegemini.com','Shruti','Arya','shrutiaraya@capegemini.com');
+INSERT INTO `` (`company_name`,`website`,`first_name`,`last_name`,`email`) VALUES ('DLF India','https://www.dlfindia.com','Sushil','Thakur','sushil.thakur@dlfindia.com');
+INSERT INTO `` (`company_name`,`website`,`first_name`,`last_name`,`email`) VALUES ('BHEL','https://www.bhel.co.in','Ramesh','Kumar','ramesh.yadav@bhel.co.in');
+INSERT INTO `` (`company_name`,`website`,`first_name`,`last_name`,`email`) VALUES ('UBS India','https://www.ubsindia.co.in','Deeksha','yadav','deeksha@ubsindia.co.in');
+INSERT INTO `` (`company_name`,`website`,`first_name`,`last_name`,`email`) VALUES ('Hero','https://www.hero.com','Mariya','Paul','mariya.paul@hero.com');
+INSERT INTO `` (`company_name`,`website`,`first_name`,`last_name`,`email`) VALUES ('Boat India','https://www.boatindia.com','Nitin','Bansal','nitin@boatindia.com');
+INSERT INTO `` (`company_name`,`website`,`first_name`,`last_name`,`email`) VALUES ('Samsung','https://www.samsung.co.in','Ruchi','Soni','ruchi.soni@samsung.co.in');
